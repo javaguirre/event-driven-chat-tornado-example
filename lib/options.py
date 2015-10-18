aa56740ef6b9138a9fb4692af59bf5ab879e8bbb
Javier Aguirre <contacto@javaguirre.net>
2015-10-18 19:15:49 +0200

    Added backends to tornado app options

--- lib/options.py
+++ lib/options.py
@@ -1,10 +1,17 @@
 import yaml
 
+from lib.backend import RedisBackend, RedisMobileBackend
+
 
 def get_options():
     data = {}
 
     with open('../options.yml', 'r') as f:
         data = yaml.load(f)
+
+    # TODO We could get redis or other backend data
+    # from the yml file
+    data['backend'] = RedisBackend()
+    data['mobile_backend'] = RedisMobileBackend()
 
     return data
